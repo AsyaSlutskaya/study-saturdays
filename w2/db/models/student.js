f831baae85e6cb3270e7c6e8f82151aa3d4792c1
asyaslutskaya <asyasutskaya@gmail.com>
2018-08-11 11:45:56 -0400

    done

--- w2/db/models/student.js
+++ w2/db/models/student.js
@@ -4,4 +4,30 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 
+const Student = db.define('Student', {
+  firstName: {
+    type : Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+}
+});
+
+Student.beforeCreate((student) => {
+  student.firstName[0] = student.firstName[0].toUpperCase();
+  student.lastName[0] = student.lastName[0].toUpperCase();
+});
+
+
+
+
 module.exports = Student;
